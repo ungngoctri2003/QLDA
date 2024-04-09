a4ebd497d614285f733f08e6ae85ac4460a05479
Ứng Trí <lamcongtri200@gmail.com>
2024-04-09 15:55:45 +0700

    QLDA1 update 9

--- src/pages/Admin/Banner/Create/index.jsx
+++ src/pages/Admin/Banner/Create/index.jsx
@@ -80,7 +80,14 @@ export default function CreateBanner() {
             alt={`${srcImg}...`}
           />
         </Form.Item>
-        ư
+        <div className="text-center ">
+          <button
+            className="border bg-sky-300 text-white border-white px-5 py-2 rounded"
+            type="submit"
+          >
+            Thêm
+          </button>
+        </div>
       </Form>
     </div>
   );
